ceb03d88925e689c27090e8630f448f7c90bd180
midas87 <makaveliddon87@gmail.com>
2022-10-24 10:51:21 -0400

    added a code to get the unique IP adddress in the list

--- IP_Verification.py
+++ IP_Verification.py
@@ -1,7 +1,6 @@
 import os
 import re
 
-import setuptools.dist
 
 ipList = "20.189.172.161", "40.126.29.11", "40.126.29.12", "52.168.116.128", "52.182.141.192", "159.203.55.55", \
          "184.168.130.131", "192.99.195.220", "204.79.197.200", "208.91.248.3", "208.91.248.3", "6.137.90.51"
@@ -18,12 +17,6 @@ with open(r"C:\Users\htukuru\OneDrive - Alithya\Desktop\ipchecklist.txt", 'r') a
     ipread = ipFile.readlines()
     for read in ipread:
         newIpList.append(read.strip())
-
-    # print(newIpList)
-
-    # for read in newIpList:
-    #     if read not in ip:
-            # print(read)
 
 path = r"C:\Users\htukuru\Downloads\dns_entry"
 
@@ -42,25 +35,11 @@ for path, dirs, files in os.walk(path):
                     print(f"IP found from {fullPath}", reads)
                     allIps.append(reads)
 
-            # allIps.append(reFile)
-
-
 print(allIps)
 
-
-uniqIP = []
+uniqIPSet = set()
 
 for uni in allIps:
-    if uni in allIps:
-        uniqIP.append(uni)
+    uniqIPSet.add(uni)
 
-
-print(uniqIP)
-# txtFile = input("Please put the text filename here: ")
-#
-# with open(txtFile, "r") as file:
-#     filename = file.read()
-#     # print(filename)
-#     reFile = re.findall(r"\d+\.\d+\.\d+\.\d+", filename)
-#
-#     print(reFile)
+print(uniqIPSet)
