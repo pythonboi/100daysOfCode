dcf38afb2ff0b00247ea251b0faec9e39d268b49
midas87 <makaveliddon87@gmail.com>
2022-12-27 22:13:47 -0500

    add the straight lines and drawing the stars

--- GhanaFlag.py
+++ GhanaFlag.py
@@ -10,24 +10,64 @@ sc = Screen()
 
 tt = Turtle()
 
+tt.pensize(2)
+tt.hideturtle()
+
+lines = [-100, 15, 100, 30]
+
 
 def moveForward():
-    pass
+    tt.penup()
+    tt.goto(-100, 80)
+    tt.pendown()
+    tt.forward(300)
+    tt.right(90)
+    tt.forward(200)
+    tt.right(90)
+    tt.forward(300)
+    tt.right(90)
+    tt.forward(200)
 
 
-tt.pensize(2)
+moveForward()
+
+
+def drawMiddle():
+    tt.penup()
+    tt.goto(-100, 15)
+    tt.right(90)
+    tt.pendown()
+    tt.forward(300)
+
+
+drawMiddle()
+
+
+def drawMiddlen():
+    tt.penup()
+    tt.goto(-100, -55)
+    # tt.right(90)
+    tt.pendown()
+    tt.forward(300)
+
+
+drawMiddlen()
 
 tt.penup()
-tt.goto(-100, 80)
-tt.pendown()
-tt.forward(300)
-tt.right(90)
-tt.forward(200)
-tt.right(90)
-tt.forward(300)
-tt.right(90)
-tt.forward(200)
+tt.goto(20, -10)
 
-tt.penup()
+
+def star():
+    tt.fillcolor("black")
+    tt.begin_fill()
+    for drawStar in range(5):
+        tt.pendown()
+        tt.forward(75)
+        tt.right(144)
+
+
+tt.end_fill()
+
+star()
 
 sc.exitonclick()
