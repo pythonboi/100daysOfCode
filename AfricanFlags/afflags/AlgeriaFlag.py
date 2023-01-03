28f8f2eb6b37ffee27ab509aaca9a10716f5a7ab
midas87 <makaveliddon87@gmail.com>
2023-01-02 22:09:09 -0500

    add the star and fix the bug for the half cricle inside the inner circle

--- AfricanFlags/afflags/AlgeriaFlag.py
+++ AfricanFlags/afflags/AlgeriaFlag.py
@@ -8,8 +8,7 @@ sc = Screen()
 tl.penup()
 tl.pensize(2)
 tl.speed(8)
-
-full = 360
+#tl.hideturtle()
 
 
 # Draw the square flag shape
@@ -26,9 +25,7 @@ def square():
     tl.forward(350)
 
 
-# square()
-
-tl.color("#004d00")
+tl.color("green")
 
 
 def dkgreen():
@@ -48,18 +45,107 @@ def crescent():
     tl.pendown()
     tl.begin_fill()
     for i in range(1, 1 + 1, 1):
-
-        tl.circle(90 * i)
+        tl.circle(108 * i)
 
         tl.end_fill()
 
+        # This is the inner circle
         for m in range(1, 1 + 1, 1):
             tl.color("white")
-            # tl.begin_fill()
-            tl.circle(70 * i)
-            # tl.end_fill()
+            tl.begin_fill()
+            tl.circle(77 * i)
+            tl.end_fill()
+
+            # Before the half circle
+
+            tl.penup()
+            print(tl.position())
+            tl.goto(0, -110)
+            tl.left(90)
+            tl.color("green")
+            tl.begin_fill()
+
+            # Creating the half circle
+            for w in range(180):
+                tl.pendown()
+
+                tl.forward(1.3)
+                tl.right(1)
+            tl.right(90)
+            tl.forward(115)
+            tl.end_fill()
+            tl.penup()
 
 
 crescent()
+
+# tl.goto(-0, 35)
+#
+# tl.penup()
+#
+# tl.color("green")
+#
+#
+# def straight():
+#     tl.left(90)
+#     tl.forward(40)
+#     tl.left(90)
+#     tl.forward(150)
+#     tl.left(90)
+#     tl.forward(40)
+#
+#
+# tl.begin_fill()
+# straight()
+# tl.end_fill()
+
+print(tl.position())
+
+tl.goto(60, -10)
+tl.color("red")
+
+
+def star():
+    tl.begin_fill()
+    tl.penup()
+    tl.right(15)
+    for mn in range(5):
+
+        tl.pendown()
+
+        tl.forward(80)
+        tl.right(145)
+
+
+star()
+tl.end_fill()
+
+tl.penup()
+
+print(tl.pos())
+
+tl.goto(90, 50)
+
+tl.color("pink")
+
+tl.begin_fill()
+tl.circle(80)
+tl.end_fill()
+
+
+# tl.goto(140, -180)
+#
+# tl.left(90)
+#
+# for x in range(180):
+#     tl.color("blue")
+#     tl.begin_fill()
+#     tl.forward(1)
+#     tl.right(1)
+#
+#
+# tl.right(90)
+# tl.forward(115)
+# tl.end_fill()
 
 sc.exitonclick()
