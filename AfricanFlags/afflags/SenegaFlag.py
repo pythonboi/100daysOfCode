4cdfa733e47ee0ac0351e520d96ea4180ee309d1
midas87 <makaveliddon87@gmail.com>
2023-01-06 23:27:51 -0500

    added new code drawing Senegal flag

---
+++ AfricanFlags/afflags/SenegaFlag.py
@@ -0,0 +1,86 @@
+from turtle import Turtle
+from turtle import Screen
+
+sc = Screen()
+tt = Turtle()
+
+sc.bgcolor("black")
+
+tt.penup()
+tt.hideturtle()
+tt.speed(10)
+
+
+def square():
+    tt.pendown()
+
+    tt.right(180)
+    tt.forward(500)
+    tt.left(90)
+    tt.forward(250)
+    tt.left(90)
+    tt.forward(500)
+    tt.left(90)
+    # print(tt.xcor(), tt.ycor())
+    print(tt.pos())
+    tt.forward(250)
+
+
+square()
+
+
+def green():
+    tt.penup()
+    tt.color("green")
+    tt.goto(-400, 250)
+    # tt.pendown()
+    square()
+
+
+tt.begin_fill()
+green()
+tt.end_fill()
+
+
+def yellow():
+    tt.color("yellow")
+    tt.penup()
+    tt.right(90)
+    tt.goto(-150, 250)
+    square()
+
+
+tt.begin_fill()
+yellow()
+tt.end_fill()
+
+
+def red():
+    tt.color("red")
+    tt.penup()
+    tt.right(90)
+    tt.goto(100, 250)
+    square()
+
+
+tt.begin_fill()
+red()
+tt.end_fill()
+
+
+def star():
+    tt.color("green")
+    tt.penup()
+    tt.goto(15, -50)
+    tt.right(35)
+    for n in range(5):
+        tt.forward(135)
+        tt.right(144)
+
+
+tt.begin_fill()
+star()
+tt.end_fill()
+
+
+sc.exitonclick()
